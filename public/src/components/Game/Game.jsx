0b574a4bb13e511eb1ffcaa04a8e5470166f88a3
clement <clement22860@gmail.com>
2017-08-22 16:28:01 +0200

    begining integration of the game

--- public/src/components/Game/Game.jsx
+++ public/src/components/Game/Game.jsx
@@ -1,17 +1,45 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
 
+const WEBSOCKETS_URL = process.env.WEBSOCKETS_URL;
+
 /**
  * Game component
  */
 class Game extends Component {
+	/**
+	 * Hook called after component rendered
+	 */
+	componentDidMount() {
+		const gameRules = {};
+		fetch(`${process.env.API_URL}/grounds`)
+			.then((response) => response.json())
+			.then((grounds) => {
+				gameRules.grounds = grounds;
+			})
+			.catch((error) => logger.error(error));
+		const config = {
+			divIdName:   'game',
+			height:      640,
+			width:       1280,
+			tokenPlayer: process.env.TOKEN_PLAYER,
+			shortIdRoom: process.env.SHORTID_ROOM,
+			socketUrl:   WEBSOCKETS_URL,
+			gameRules,
+		};
+		const landsWar = new LandsWar(config);
+		landsWar.start().catch((error) => {
+			logger.error(error);
+		});
+	}
+
 	/**
  	* render
 	* @returns {JSX} return jsx
  	*/
 	render() {
 		return (
-            <h2>Game to include in /src/components/Game/Game.jsx</h2>
+			<div id="game"></div>
 		);
 	}
 }
